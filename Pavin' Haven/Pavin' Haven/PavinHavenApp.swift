3b3466762d70bff21b3a60f626036bb25985b608
johnsonza <55718475+johnsonza@users.noreply.github.com>
2022-03-18 14:08:39 -0500

    Initial Commit

---
+++ Pavin' Haven/Pavin' Haven/PavinHavenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pavin__HavenApp.swift
+//  Pavin' Haven
+//
+//  Created by Zachary Johnson on 3/18/22.
+//
+
+import SwiftUI
+
+@main
+struct PavinHavenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
